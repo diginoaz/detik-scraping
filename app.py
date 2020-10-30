f08ae97416168d16e133dd9071dd5c531eb1954a
cakdion <ajatotindo@gmail.com>
2020-10-30 13:28:11 +0700

    scraping detik popular

---
+++ app.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+html_doc = requests.get('https://www.detik.com/terpopuler', params={'tag_from=wp': 'wp_cb_mostPopular_more'})
+soup = BeautifulSoup(html_doc.text, 'html.parser')
+
+print(soup)
