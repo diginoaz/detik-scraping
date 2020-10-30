c1c0f13fd1d2aada1a203176fd258f531e354ff9
cakdion <ajatotindo@gmail.com>
2020-10-30 16:45:08 +0700

    scraping detik popular

--- app.py
+++ app.py
@@ -3,5 +3,12 @@ from bs4 import BeautifulSoup
 
 html_doc = requests.get('https://www.detik.com/terpopuler', params={'tag_from=wp': 'wp_cb_mostPopular_more'})
 soup = BeautifulSoup(html_doc.text, 'html.parser')
+populer_area = soup.find(attrs={'class': 'grid-row list-content'})
+titles = populer_area.findAll(attrs={'class': 'media__title'})
+images = populer_area.findAll(attrs={'class': 'media__image'})
 
-print(soup)
+for image in images:
+    print(image.find('a').find('img')['title'])
+
+#print(titles)
+
